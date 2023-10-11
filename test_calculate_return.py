9045349beb1cc768b5af8db5fc68556f32aa6a99
HA <hedgar@HAs-MacBook-Pro.local>
2023-10-11 16:51:22 -0700

    adjustedcpicalc

--- test_calculate_return.py
+++ test_calculate_return.py
@@ -7,8 +7,8 @@ from calculate_real_return.stock_calculator import (
 def test_correct_calculations():
     calc = StockCalculator(100, "01/01/2021", 10, 2, 100, 5)  # sample input
     min_price, max_price = calc.calculate_sell_price()
-    assert min_price == 123.88129655096716
-    assert max_price == 130.3556221528691
+    assert min_price == 124.2999182010532
+    assert max_price == 131.91216664190983
 
 
 def test_edge_cases():
