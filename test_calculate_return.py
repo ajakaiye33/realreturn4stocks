4c761d31776b65b464269ad977c7abb51eb707d1
HA <hedgar@HAs-MacBook-Pro.local>
2023-10-11 18:12:18 -0700

    pushna

--- test_calculate_return.py
+++ test_calculate_return.py
@@ -7,7 +7,7 @@ from calculate_real_return.stock_calculator import (
 def test_correct_calculations():
     calc = StockCalculator(100, "01/01/2021", 10, 2, 100, 5)  # sample input
     min_price, max_price = calc.calculate_sell_price()
-    assert min_price == 124.2999182010532
+    assert min_price == 124.3265148405673
     assert max_price == 131.91216664190983
 
 
